b3eae4c4ac3456335a12a83ec5200cc2e53d8e5b
Vladyslav Tyrin <tyrin7@gmail.com>
2016-03-19 02:58:43 +0100

    Init

---
+++ AirControllerWebService/Controllers/REST/GetCO2LastValueController.cs
@@ -0,0 +1,27 @@
+﻿using AirControllerWebService.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AirControllerWebService.Controllers.REST
+{
+    public class GetCO2LastValueController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var value = ManageDB.GetCO2LastValue();
+
+                return Request.CreateResponse(HttpStatusCode.OK, value);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
